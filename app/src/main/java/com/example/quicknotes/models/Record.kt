8eb2611fb1b77949d1aea9667f323f4d674eeddc
galij899 <47144051+galij899@users.noreply.github.com>
2021-05-25 18:51:32 +0300

    working listview

--- app/src/main/java/com/example/quicknotes/models/Record.kt
+++ app/src/main/java/com/example/quicknotes/models/Record.kt
@@ -1,4 +1,11 @@
 package com.example.quicknotes.models
 
-class Record {
+class Record(
+    val title: String,
+    val author: String,
+    val text: String
+) {
+    override fun toString(): String {
+        return "Record [title: ${this.title}, author: ${this.author}, text: ${this.text}]"
+    }
 }
--- app/src/main/java/com/example/quicknotes/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/example/quicknotes/ui/notes/NotesFragment.kt
@@ -1,16 +1,33 @@
 package com.example.quicknotes.ui.notes
 
+import android.R.layout
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.example.quicknotes.R
-import com.example.quicknotes.databinding.FragmentHomeBinding
+import android.widget.ListView
+import android.widget.ArrayAdapter
 import com.example.quicknotes.databinding.NotesFragmentBinding
-import com.example.quicknotes.ui.home.HomeViewModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.example.quicknotes.models.Record
+import kotlin.reflect.typeOf
+
+fun readJson(): List<Record> {
+    val jsonList =
+        """[{"title": "Kotlin Tutorial", "author": "bezkoder", "text" : "sometext"},
+            {"title": "Kotlin Tutorial", "author": "bezkoder", "text" : "sometext"},
+            {"title": "Kotlin Tutorial", "author": "bezkoder", "text" : "sometext"}]""".trimMargin()
+
+    val gson = Gson()
+
+    val listRecordType = object : TypeToken<List<Record>>() {}.type
+    var records: List<Record> = gson.fromJson(jsonList, listRecordType)
+
+    return records
+}
 
 class NotesFragment : Fragment() {
 
@@ -20,16 +37,32 @@ class NotesFragment : Fragment() {
 
     private lateinit var viewModel: NotesViewModel
 
-    fun read_json(): {}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {_binding = NotesFragmentBinding.inflate(inflater, container, false)
+    ): View? {
+        _binding = NotesFragmentBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.sampleText
-        textView.text = "lololo"
+        val listView: ListView = binding.recordList
+
+        val recordArray = readJson()
+
+        val listItems = arrayOfNulls<String>(recordArray.size)
+
+        for (i in 0 until recordArray.size) {
+            val record = recordArray[i]
+            listItems[i] = record.title
+        }
+
+        val adapter = ArrayAdapter(
+            activity!!,
+            android.R.layout.simple_list_item_1,
+            listItems
+        )
+        listView.adapter = adapter
+
         return root
     }
 
